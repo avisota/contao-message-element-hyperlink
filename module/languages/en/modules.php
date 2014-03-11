159b0a6d7b062ce41cafe86cd8183b205da08ece
Tristan Lins <tristan.lins@bit3.de>
2014-03-11 15:56:44 +0100

    Add module description.

---
+++ module/languages/en/modules.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Avisota newsletter and mailing system
+ * Copyright (C) 2013 Tristan Lins
+ *
+ * PHP version 5
+ *
+ * @copyright  bit3 UG 2013
+ * @author     Tristan Lins <tristan.lins@bit3.de>
+ * @package    avisota/contao-message-element-hyperlink
+ * @license    LGPL-3.0+
+ * @filesource
+ */
+
+
+/**
+ * Module
+ */
+$GLOBALS['TL_LANG']['MOD']['avisota-message-element-hyperlink'] = array(
+	'Avisota - Message element "Hyperlink"',
+	'"Hyperlink" message element for Avisota messages.'
+);
